7e4640ebd2e88ffa658036d24347802aacb737bc
Debajyoti Debnath <debajyotid2@hotmail.com>
2025-07-29 22:25:06 -0400

    Change build script to exit on failure

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,6 +11,7 @@ if [ -d "$BUILD_DIR" ]; then
     rm -rf $BUILD_DIR
 fi
 
-mkdir $BUILD_DIR && cd $BUILD_DIR || exit 1
-cmake .. || exit 1
+set -e
+mkdir $BUILD_DIR && cd $BUILD_DIR
+cmake ..
 cmake --build . -j "$NUM_THREADS"
