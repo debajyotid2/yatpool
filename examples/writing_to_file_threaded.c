ddcd5530efe5102f7be56ea215395398216feb16
Debajyoti Debnath <debajyotid2@hotmail.com>
2025-07-30 13:03:56 -0400

    Further optimize writing to file example by calculating offsets while generating data.

--- examples/writing_to_file_threaded.c
+++ examples/writing_to_file_threaded.c
@@ -1,5 +1,5 @@
 /* Generating data (random numbers) and writing to a csv file in parallel
- 
+
     YATPool - Yet Another Thread Pool implemented in C
 
     Copyright (C) 2024  Debajyoti Debnath
@@ -19,131 +19,112 @@
 
 */
 
-#include <stdio.h>
-#include <stdlib.h> 
-#include <string.h>
-#include <sys/types.h>
+#include "yatpool.h"
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
-#include <unistd.h>
-#include <time.h>
 #include <sys/time.h>
-#include "yatpool.h"
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
 
-#define NCOLS 100       // Number of columns in each output file line
-#define MAX_BUFLEN 10   // Maximum number of digits in each int to generate
+#define NCOLS 100     // Number of columns in each output file line
+#define MAX_BUFLEN 10 // Maximum number of digits in each int to generate
 
 typedef struct {
-    char* data;
+    char *data;
     size_t length;
 } String;
 
 typedef struct {
     size_t lineno;
-    String* line;
+    String *line;
 } Line;
 
 typedef struct {
-    Line** lines;
+    Line **lines;
     size_t start_lineno;
     size_t end_lineno;
+    size_t *offset_ptr;
 } GenerateLinesArg;
 
 typedef struct {
-    Line** lines;
-    size_t start_lineno;
-    size_t end_lineno;
-    size_t* offset_ptr;
-} GetOffsetArg;
-
-typedef struct {
-    char* mapped_file;
-    Line** lines;
+    char *mapped_file;
+    Line **lines;
     size_t start_lineno;
     size_t end_lineno;
     size_t offset;
 } WriteToFileArg;
 
-String string_create(const char* str, size_t length) {
-    if (str == NULL) { 
+String string_create(const char *str, size_t length) {
+    if (str == NULL) {
         fprintf(stderr, "Null string provided.\n");
         abort();
     }
     String res;
     res.length = length;
-    res.data = (char*)calloc(length+1, sizeof(char));
+    res.data = (char *)calloc(length + 1, sizeof(char));
     strncpy(res.data, str, length);
     res.data[length] = '\0';
     return res;
 }
 
-void string_destroy(String* str) {
-    if (str==NULL) return;
+void string_destroy(String *str) {
+    if (str == NULL)
+        return;
     free(str->data);
 }
 
-void string_print(const String* str) {
-    if (str==NULL) return;
+void string_print(const String *str) {
+    if (str == NULL)
+        return;
     printf("%s\n", str->data);
 }
 
-void line_init(Line** line, size_t lineno) {
-    if (line==NULL) return;
-    *line = (Line*)malloc(sizeof(Line));
-    (*line)->line = (String*)malloc(sizeof(String));
+void line_init(Line **line, size_t lineno) {
+    if (line == NULL)
+        return;
+    *line = (Line *)malloc(sizeof(Line));
+    (*line)->line = (String *)malloc(sizeof(String));
     (*line)->lineno = lineno;
 }
 
-void line_destroy(Line* line) {
-    if (line==NULL) return;
+void line_destroy(Line *line) {
+    if (line == NULL)
+        return;
     string_destroy(line->line);
     free(line->line);
 }
 
-void generatelinesarg_init(GenerateLinesArg** arg, Line** lines, size_t start_lineno, size_t end_lineno) {
-    if (arg==NULL || lines==NULL) return;
-    GenerateLinesArg** _arg = (GenerateLinesArg**)arg;
-    *_arg = (GenerateLinesArg*)malloc(sizeof(GenerateLinesArg));
+void generatelinesarg_init(GenerateLinesArg **arg, Line **lines,
+                           size_t start_lineno, size_t end_lineno,
+                           size_t *offset_ptr) {
+    if (arg == NULL || lines == NULL)
+        return;
+    GenerateLinesArg **_arg = (GenerateLinesArg **)arg;
+    *_arg = (GenerateLinesArg *)malloc(sizeof(GenerateLinesArg));
     (*_arg)->lines = lines;
     (*_arg)->start_lineno = start_lineno;
     (*_arg)->end_lineno = end_lineno;
+    (*_arg)->offset_ptr = offset_ptr;
 }
 
-void generatelinesarg_destroy(void* arg) {
-    if (arg==NULL) return;
-    GenerateLinesArg* _arg = (GenerateLinesArg*)arg;
+void generatelinesarg_destroy(void *arg) {
+    if (arg == NULL)
+        return;
+    GenerateLinesArg *_arg = (GenerateLinesArg *)arg;
     free(_arg);
 }
 
-void getoffsetarg_init(GetOffsetArg** arg, 
-                       Line** lines, 
-                       size_t start_lineno,
-                       size_t end_lineno,
-                       size_t* offset_ptr) {
-    if (arg==NULL) return;
-    if (lines==NULL) return;
-    *arg = (GetOffsetArg*)malloc(sizeof(GetOffsetArg));
-    (*arg)->lines = lines;
-    (*arg)->start_lineno = start_lineno;
-    (*arg)->end_lineno = end_lineno;
-    (*arg)->offset_ptr = offset_ptr;
-}
-
-void getoffsetarg_destroy(void* arg) {
-    if (arg==NULL) return;
-    GetOffsetArg* _arg = (GetOffsetArg*)arg;
-    free(_arg);
-}
-
-void writetofilearg_init(WriteToFileArg** arg,
-                         char* mapped,
-                         Line** lines,
-                         size_t start_lineno,
-                         size_t end_lineno,
+void writetofilearg_init(WriteToFileArg **arg, char *mapped, Line **lines,
+                         size_t start_lineno, size_t end_lineno,
                          size_t offset) {
-    if (arg==NULL || mapped==NULL || lines==NULL) return;
-    *arg = (WriteToFileArg*)malloc(sizeof(WriteToFileArg));
+    if (arg == NULL || mapped == NULL || lines == NULL)
+        return;
+    *arg = (WriteToFileArg *)malloc(sizeof(WriteToFileArg));
     (*arg)->mapped_file = mapped;
     (*arg)->lines = lines;
     (*arg)->start_lineno = start_lineno;
@@ -151,78 +132,76 @@ void writetofilearg_init(WriteToFileArg** arg,
     (*arg)->offset = offset;
 }
 
-void writetofilearg_destroy(void* arg) {
-    if (arg==NULL) return;
-    WriteToFileArg* _arg = (WriteToFileArg*)arg;
+void writetofilearg_destroy(void *arg) {
+    if (arg == NULL)
+        return;
+    WriteToFileArg *_arg = (WriteToFileArg *)arg;
     free(_arg);
 }
 
 /// Function for threadpool to generate a bunch of lines of data
-void* generate_lines(void* arg) {
-    GenerateLinesArg* lnarg = (GenerateLinesArg*)arg;
-    unsigned int seed = time(NULL) ^ lnarg->start_lineno;   // Unique seed
-    
+void *generate_lines(void *arg) {
+    GenerateLinesArg *lnarg = (GenerateLinesArg *)arg;
+    unsigned int seed = time(NULL) ^ lnarg->start_lineno; // Unique seed
+
     // Create a line buffer to store a single line
     // Each number takes MAX_BUFLEN+1(comma)
     // Each line takes NCOLS*(MAX_BUFLEN+1)+2(\n + \0)
-    size_t max_line_len = NCOLS*(MAX_BUFLEN+1)+2;
-    char* line_buffer = (char*)calloc(max_line_len, sizeof(char));
-    
-    for (size_t i=lnarg->start_lineno; i<lnarg->end_lineno; ++i) {
-        line_init(&(lnarg->lines[i]), i+1);
+    size_t max_line_len = NCOLS * (MAX_BUFLEN + 1) + 2;
+    char *line_buffer = (char *)calloc(max_line_len, sizeof(char));
+    size_t total_line_length = 0;
 
-        char* ptr = line_buffer;
+    for (size_t i = lnarg->start_lineno; i < lnarg->end_lineno; ++i) {
+        line_init(&(lnarg->lines[i]), i + 1);
+
+        char *ptr = line_buffer;
         size_t buflen = 0;
-        for (int i=0; i<NCOLS; ++i) {
-            int num_bytes_written = sprintf(ptr, "%d,", rand_r(&seed)%NCOLS);
+        for (int i = 0; i < NCOLS; ++i) {
+            int num_bytes_written = sprintf(ptr, "%d,", rand_r(&seed) % NCOLS);
             ptr += num_bytes_written;
             buflen += num_bytes_written;
         }
-        line_buffer[buflen-1] = '\n';   // Last comma is replaced with a newline
+        line_buffer[buflen - 1] = '\n'; // Last comma is replaced with a newline
         *(lnarg->lines[i]->line) = string_create(line_buffer, buflen);
+        total_line_length += buflen;
     }
+    *(lnarg->offset_ptr) = total_line_length;
 
     free(line_buffer);
     return NULL;
 }
 
-/// Function for threadpool to calculate the offset required for a group of lines
-void* get_offset(void* arg) {
-    GetOffsetArg* offsetarg = (GetOffsetArg*)arg;
-    *(offsetarg->offset_ptr) = 0;
-    for (size_t i = offsetarg->start_lineno; i < offsetarg->end_lineno; ++i)
-        *(offsetarg->offset_ptr) += offsetarg->lines[i]->line->length;
-    return NULL;
-}
-
 /// Function for threadpool to write a chunk of lines into file
-void* write_to_file(void* arg) {
-    WriteToFileArg* currarg = (WriteToFileArg*)arg;
+void *write_to_file(void *arg) {
+    WriteToFileArg *currarg = (WriteToFileArg *)arg;
 
     size_t running_total_bytes = 0;
     currarg->mapped_file += currarg->offset;
 
     for (size_t i = currarg->start_lineno; i < currarg->end_lineno; ++i) {
-        memcpy(currarg->mapped_file+running_total_bytes, currarg->lines[i]->line->data, currarg->lines[i]->line->length);
+        memcpy(currarg->mapped_file + running_total_bytes,
+               currarg->lines[i]->line->data, currarg->lines[i]->line->length);
         running_total_bytes += currarg->lines[i]->line->length;
     }
     return NULL;
 }
 
 /// Comparison function for two Line objects in qsort
-int cmp_lines(const void* a, const void* b) {
-    Line* line_a = *(Line**)a;
-    Line* line_b = *(Line**)b;
+int cmp_lines(const void *a, const void *b) {
+    Line *line_a = *(Line **)a;
+    Line *line_b = *(Line **)b;
     return ((int)(line_a->lineno) - (int)(line_b->lineno));
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <file to write to> <number of lines to write>\n", argv[0]);
+        fprintf(stderr,
+                "Usage: %s <file to write to> <number of lines to write>\n",
+                argv[0]);
         return EXIT_FAILURE;
     }
     int num_lines = atoi(argv[2]);
-    
+
     if (num_lines < 1) {
         fprintf(stderr, "Must specify at least one line to write.\n");
         return EXIT_FAILURE;
@@ -231,81 +210,70 @@ int main(int argc, char** argv) {
     size_t num_threads = 8;
 
     struct timeval start, end;
-    long duration = 0;  // microseconds
-    
+    long duration = 0; // microseconds
+
     gettimeofday(&start, NULL);
 
     size_t queue_size = num_threads * 8;
 
-    YATPool* pool = yatpool_init(num_threads, queue_size);
-   
+    YATPool *pool = yatpool_init(num_threads, queue_size);
+
     // Generate data in parallel
-    Line** generated = (Line**)calloc(num_lines, sizeof(Line*));
-    
+    Line **generated = (Line **)calloc(num_lines, sizeof(Line *));
+
     size_t fac = 8 * num_threads;
     size_t num_tasks = num_lines / fac;
-    num_tasks = num_lines % fac == 0 ? num_tasks: num_tasks + 1;
-        
-    for(int i = 0; i < (int)num_tasks; ++i) {
-        GenerateLinesArg* arg;
+    num_tasks = num_lines % fac == 0 ? num_tasks : num_tasks + 1;
+
+    // Array to calculate offsets for each group of lines to write
+    size_t *offsets = (size_t *)calloc(num_tasks, sizeof(size_t));
+
+    for (int i = 0; i < (int)num_tasks; ++i) {
+        GenerateLinesArg *arg;
 
         size_t start_lineno = fac * i;
         size_t end_lineno = fac * (i + 1);
-        end_lineno = end_lineno > (size_t)num_lines? num_lines: end_lineno;
-        generatelinesarg_init(&arg, generated, start_lineno, end_lineno);
-        
+        end_lineno = end_lineno > (size_t)num_lines ? num_lines : end_lineno;
+        generatelinesarg_init(&arg, generated, start_lineno, end_lineno,
+                              &offsets[i]);
+
         yatpool_put(pool, &generate_lines, arg, &generatelinesarg_destroy);
     }
     yatpool_wait(pool);
-    
+
     // Sort data so that it is in the correct order
-    qsort(generated, num_lines, sizeof(Line*), cmp_lines);
-    
+    qsort(generated, num_lines, sizeof(Line *), cmp_lines);
+
     gettimeofday(&end, NULL);
-    duration = (end.tv_sec-start.tv_sec)*1000000+\
-                    (end.tv_usec-start.tv_usec);
-    printf("Generating data took %g milliseconds.\n", (double)duration / 1000.0);
+    duration =
+        (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
+    printf("Generating data took %g milliseconds.\n",
+           (double)duration / 1000.0);
 
     gettimeofday(&start, NULL);
 
-    num_tasks = num_lines / fac;
-    num_tasks = num_lines % fac == 0 ? num_tasks: num_tasks + 1;
-    
-    // Calculate offsets for each group of lines to write
-    size_t* offsets = (size_t*)calloc(num_tasks, sizeof(size_t));
-    memset(offsets, 0, num_tasks * sizeof(size_t));
-
-    for (size_t i = 0; i < num_tasks; ++i) {
-        GetOffsetArg* arg;
-        size_t start_lineno = fac * i;
-        size_t end_lineno = fac * (i + 1);
-        end_lineno = end_lineno > (size_t)num_lines? num_lines: end_lineno;
-        getoffsetarg_init(&arg, generated, start_lineno, end_lineno, &offsets[i]);
-
-        yatpool_put(pool, &get_offset, arg, &getoffsetarg_destroy);
+    // Array to calculate offsets for each group of lines to write
+    for (size_t i = 1; i < num_tasks; ++i) {
+        offsets[i] += offsets[i - 1];
     }
-
-    yatpool_wait(pool);
-
-    for (size_t i=1; i<num_tasks; ++i)
-        offsets[i] += offsets[i-1];
 
     // Write data in parallel to file
     int fd = open(argv[1], O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd==-1) {
+    if (fd == -1) {
         fprintf(stderr, "Could not open file %s.\n", argv[1]);
         return EXIT_FAILURE;
     }
 
     // Set file size
-    size_t file_size = offsets[num_tasks-1];
-    if (ftruncate(fd, file_size)==-1) {
+    size_t file_size = offsets[num_tasks - 1];
+    if (ftruncate(fd, file_size) == -1) {
         fprintf(stderr, "Error truncating file to specified length.\n");
         close(fd);
         return EXIT_FAILURE;
     }
 
-    char* file_buf = (char*)mmap(NULL, file_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    char *file_buf = (char *)mmap(NULL, file_size, PROT_READ | PROT_WRITE,
+                                  MAP_SHARED, fd, 0);
     if (file_buf == MAP_FAILED) {
         fprintf(stderr, "Error in mmap to file.\n");
         close(fd);
@@ -313,12 +281,13 @@ int main(int argc, char** argv) {
     }
 
     for (size_t i = 0; i < num_tasks; ++i) {
-        WriteToFileArg* arg;
+        WriteToFileArg *arg;
         size_t start_lineno = fac * i;
         size_t end_lineno = fac * (i + 1);
-        end_lineno = end_lineno > (size_t)num_lines? num_lines: end_lineno;
-        size_t offset = (i == 0)? 0: offsets[i-1];
-        writetofilearg_init(&arg, file_buf, generated, start_lineno, end_lineno, offset);
+        end_lineno = end_lineno > (size_t)num_lines ? num_lines : end_lineno;
+        size_t offset = (i == 0) ? 0 : offsets[i - 1];
+        writetofilearg_init(&arg, file_buf, generated, start_lineno, end_lineno,
+                            offset);
 
         yatpool_put(pool, &write_to_file, arg, &writetofilearg_destroy);
     }
@@ -330,10 +299,10 @@ int main(int argc, char** argv) {
     close(fd);
 
     gettimeofday(&end, NULL);
-    duration = (end.tv_sec-start.tv_sec)*1000000+\
-                    (end.tv_usec-start.tv_usec);
+    duration =
+        (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
     printf("Writing data took %g milliseconds.\n", (double)duration / 1000.0);
-    
+
     // Free all memory
     free(offsets);
 
